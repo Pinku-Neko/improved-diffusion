b5868849f13a091ef2f53a984572b1594bb16dca
Pinku-Neko <henryiakvictor@gmail.com>
2023-11-27 22:33:25 +0100

    training regression

---
+++ improved_diffusion/train_reg.py
@@ -0,0 +1,83 @@
+'''
+Train regression on images
+'''
+
+import torch as th
+from torch.optim import AdamW
+from tqdm.auto import tqdm
+
+class TrainLoop:
+    def __init__(
+        self,
+        diffusion,
+        regression,
+        data,
+        batch_size,
+        lr,
+        epochs
+    ):
+        self.diffusion = diffusion
+        self.timesteps = 1000 # TODO fix this later
+        self.regression = regression
+        self.regression_dim = 128 # TODO fix this later
+        self.data = data
+        self.batch_size = batch_size
+        self.lr = lr
+        self.epochs = epochs
+        self.cur_epoch = 0
+
+        self.model_params = list(self.regression.parameters())
+        self.opt = AdamW(params=self.model_params,lr=self.lr)
+        self.crit = th.nn.MSELoss()
+        self.losses = []
+        self.cur_loss = 0.0
+        
+        self.step = 0
+        if th.cuda.is_available():
+            self.device = 'cuda'
+
+        self.regression = self.regression.to(self.device)
+
+    def get_avg_loss(self):
+        return (sum(self.losses)/len(self.losses))
+
+    def run_loop(self):
+        p_bar=tqdm(range(self.epochs),desc=f"Regression lr {self.lr}; loss: {round(self.cur_loss,6)}")
+        for epoch in p_bar:
+            # take step
+            self.step += 1
+            batch = next(self.data)[0].to(self.device) # bad coding
+            self.train_step(batch=batch)
+            # test set
+            # save model
+            if epoch % 100 == 0:
+                self.save_regression()
+            # update progress bar
+            p_bar.set_description(f"Regression lr {self.lr}; loss: {self.get_avg_loss()}")
+    
+    def train_step(self,batch):
+        # make random t samples
+        batch_size = len(batch)
+        t_samples = th.randint(0,self.timesteps,(batch_size,1),device=self.device)
+        # make q samples
+        q_samples = self.diffusion.q_sample(batch,t_samples)
+        # step on random timesteps and model with q samples as input
+        predicts = self.regression(q_samples)
+        # compute loss
+        # normalize output
+        t_samples_norm = t_samples / self.timesteps
+        loss = self.crit(predicts,t_samples_norm)
+        # backpropagation
+        self.opt.zero_grad()
+        loss.backward()
+        self.opt.step()
+        # save losses
+        self.cur_loss = loss.item()
+        self.losses.append(self.cur_loss)
+    
+    def save_regression(self):
+        th.save({"state_dict": self.regression.state_dict(), "optimizer": self.opt.state_dict},
+                f"./models/regression/reg_{self.cur_epoch}.pth")
+    
+    
+
---
+++ scripts/image_train_reg.py
@@ -0,0 +1,63 @@
+'''
+Train encoder+regression on images
+'''
+import argparse
+
+from improved_diffusion import dist_util, logger
+from improved_diffusion.image_datasets import load_data
+from improved_diffusion.script_util import (
+    diffusion_and_regression_defaults,
+    create_diffusion_regrression,
+    args_to_dict,
+    add_dict_to_argparser
+    )
+from improved_diffusion.train_reg import TrainLoop
+
+def main():
+    args = create_argparser().parse_args()
+
+    dist_util.setup_dist()
+    
+    diffusion, regression = create_diffusion_regrression(
+        **args_to_dict(args, diffusion_and_regression_defaults().keys())
+    )
+    # logger.log("creating data loader...")
+    print("creating data loader...")
+    data = load_data(
+        data_dir=args.data_dir,
+        batch_size=args.batch_size,
+        image_size=args.image_size,
+    )
+
+    # logger.log("training...")
+    print("training...")
+    TrainLoop(
+        diffusion=diffusion,
+        regression=regression,
+        data=data,
+        batch_size=args.batch_size,
+        lr=args.lr,
+        epochs=args.epochs
+    ).run_loop()
+
+
+def create_argparser():
+    defaults = dict(
+        data_dir="cifar_train",
+        image_size=32,
+        lr=1e-4,
+        batch_size=1,
+        epochs = 1000,
+        # log_interval=10,
+        # save_interval=10000,
+        # resume_checkpoint="",
+        use_fp16=False
+    )
+    defaults.update(diffusion_and_regression_defaults())
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+
+if __name__ == "__main__":
+    main()

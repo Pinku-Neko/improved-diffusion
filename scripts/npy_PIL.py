3f01edbf2511ee27288a44c3f7eae75af1e9ecbd
Pinku-Neko <henryiakvictor@gmail.com>
2024-01-03 21:38:11 +0100

    for revealing npz in pyplot

--- scripts/npy_PIL.py
+++ scripts/npy_PIL.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-dfile = 'samples\samples_16x32x32x3.npz'
+dfile = 'samples\samples_16x32x32x3_1.npz'
 images = np.load(dfile)["arr_0"]
 plt.ion()
 plt.figure()
 plt.imshow(images[0])
+import pdb; pdb.set_trace()

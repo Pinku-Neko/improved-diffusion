6818278c5d8d342bb7c40313cc7387632b7fdb03
Pinku-Neko <henryiakvictor@gmail.com>
2024-01-18 16:13:28 +0100

    toy script showing images

--- scripts/npy_PIL.py
+++ scripts/npy_PIL.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-dfile = 'samples/result.npz'
+dfile = 'samples/samples_20240105_151856_skip0.8_5000.npz'
 images = np.load(dfile)["arr_0"]
 plt.ion()
 plt.figure()
-plt.imshow(images[0])
-import pdb; pdb.set_trace()
+try:
+    for i in range(len(images)):
+        plt.imshow(images[i])
+        plt.pause(1)
+        plt.draw()
+except KeyboardInterrupt:
+    print("Execution manually stopped by the user.")
